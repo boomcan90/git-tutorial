86b45dfd5ffc2bbbc506effea8c1a5631431967e
boomcan90 <arjun_singh@mymail.sutd.edu.sg>
2015-11-13 15:14:27 +0800

    hai

--- f1.py
+++ f1.py
@@ -2,7 +2,7 @@
 """
 Create 3 functions: multiply, add, divide for the calculator class
 
-Each student should make their own branch and work on one of the functions in 
+Each student should make their own branch and work on one of the functions in
 the corresponding file.
 """
 
@@ -12,9 +12,12 @@ import division as dv
 
 
 class Calculator:
-    
-    def  __init__(self):
+
+    def __init__(self):
         return None
-        
-        
-        
+
+    def add(self, a, b):
+        return ad(a, b)
+
+    def multiply(self, a, b):
+        return ml(a, b)
